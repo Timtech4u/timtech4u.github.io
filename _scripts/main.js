78b25457a30820ef4469bdeaecb1c7b611d6be2e
Kelvin Kamau <kelvinkamau6808@gmail.com>
2018-05-16 12:27:18 +0300

    Initial commit

---
+++ _scripts/main.js
@@ -0,0 +1,77 @@
+$(function() {
+  const d = new Date();
+  const hours = d.getHours();
+  const night = hours >= 19 || hours <= 7; // between 7pm and 7am
+  const body = document.querySelector('body');
+  const toggle = document.getElementById('toggle');
+  const input = document.getElementById('switch');
+
+  if (night) {
+    input.checked = true;
+    body.classList.add('night');
+  }
+
+  toggle.addEventListener('click', function() {
+    const isChecked = input.checked;
+    if (isChecked) {
+      body.classList.remove('night');
+    } else {
+      body.classList.add('night');
+    }
+  });
+
+  const introHeight = document.querySelector('.intro').offsetHeight;
+  const topButton = document.getElementById('top-button');
+  const $topButton = $('#top-button');
+
+  window.addEventListener(
+    'scroll',
+    function() {
+      if (window.scrollY > introHeight) {
+        $topButton.fadeIn();
+      } else {
+        $topButton.fadeOut();
+      }
+    },
+    false
+  );
+
+  topButton.addEventListener('click', function() {
+    $('html, body').animate({ scrollTop: 0 }, 500);
+  });
+
+  const hand = document.querySelector('.emoji.wave-hand');
+
+  function waveOnLoad() {
+    hand.classList.add('wave');
+    setTimeout(function() {
+      hand.classList.remove('wave');
+    }, 2000);
+  }
+
+  setTimeout(function() {
+    waveOnLoad();
+  }, 1000);
+
+  hand.addEventListener('mouseover', function() {
+    hand.classList.add('wave');
+  });
+
+  hand.addEventListener('mouseout', function() {
+    hand.classList.remove('wave');
+  });
+
+  window.sr = ScrollReveal({
+    reset: false,
+    duration: 600,
+    easing: 'cubic-bezier(.694,0,.335,1)',
+    scale: 1,
+    viewFactor: 0.3,
+  });
+
+  sr.reveal('.background');
+  sr.reveal('.skills');
+  sr.reveal('.experience', { viewFactor: 0.2 });
+  sr.reveal('.featured-projects', { viewFactor: 0.1 });
+  sr.reveal('.other-projects', { viewFactor: 0.05 });
+});
---
+++ build/browsersync.js
@@ -0,0 +1,41 @@
+const browserSync  = require('browser-sync').create();
+const cp           = require('child_process');
+
+const jekyll       = process.platform === 'win32' ? 'jekyll.bat' : 'jekyll';
+
+const scssPath     = '_scss/**/*.scss';
+const jsPath       = '_scripts/*.js';
+const templatePath = ['*.html', '+(_includes|_layouts)/*.html', '*.yml', '_data/*.yml', '_posts/*'];
+
+module.exports = gulp => {
+
+  // run `jekyll build`
+  gulp.task('jekyll-build', done => {
+    return cp.spawn(jekyll, ['build'], {stdio: 'inherit'})
+      .on('close', done);
+  });
+
+  // run `jekyll build` with _config_dev.yml
+  gulp.task('jekyll-dev', done => {
+    return cp.spawn(jekyll, ['build', '--config', '_config.yml,_config_dev.yml'], {stdio: 'inherit'})
+      .on('close', done);
+  });
+
+  // Rebuild Jekyll then reload the page
+  gulp.task('jekyll-rebuild', ['jekyll-dev'], () => {
+    browserSync.reload();
+  });
+
+  gulp.task('serve', ['jekyll-dev'], () => {
+    browserSync.init({
+      server: {
+        baseDir: '_site'
+      }
+    });
+
+    gulp.watch(scssPath, ['sass', browserSync.reload]);
+    gulp.watch(jsPath, ['scripts', browserSync.reload]);
+    gulp.watch(templatePath, ['jekyll-rebuild']);
+  });
+
+};
